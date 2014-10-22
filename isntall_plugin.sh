d566e390b75dd581304f0cd45baa34bfa4e84e11
sfeng <ylsongfeng@gmail.com>
2014-10-22 13:54:11 +0800

    delete non useful shell, plugin config already configured in package.json, npm install will install plugins inneed

--- isntall_plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script install hexo plugins which used in my hexo blog
-
-# hexo-tag-bootstrap
-# https://github.com/wzpan/hexo-tag-bootstrap
-npm isntall hexo-tag-bootstrap --save
-
-
